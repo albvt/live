591e855fe97c4097f543ff810c85a91cf320958d
albvt <stainagaba@gmail.com>
2019-10-06 08:52:16 +0300

    Update homedetails.vue

--- components/landed/homedetails.vue
+++ components/landed/homedetails.vue
@@ -3,17 +3,49 @@
         <v-container grid-list-lg>
             <v-layout column>
                 <v-flex  class="display-2 font-weight-black brown--text text-xs-center my-5">How It Works</v-flex>
-                <v-flex>
+                <v-flex >
                     <div class="headline mt-3 brown--text font-weight-bold">Create An Account</div>
-                    <p class="subheading mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Euismod nibh pretium. Etiam pulvinar tincidunt dapibus. Quisque sollicitudin, mauris a consequat consectetur, turpis nisl sollicitudin enim, id consectetur neque neque nec metus. Pellentesque dolor nisi, vulputate quis lobortis ac, tincidunt et quam. Mauris pulvinar blandit nisi nec mattis. Aliquam accumsan ut sem eget efficitur.</p>
+                    <v-flex xs12 sm5 md3 justify-end>
+                      <v-card tile>
+                        <v-card-text >using your google account, or any social, link them up and get started</v-card-text>
+
+                      </v-card>
+                    </v-flex>
+
+                    <v-flex offset-sm8 xs4>
+
+                      <v-card><v-img src="http://albvt.pythonanywhere.com/media/cover/blackwhite3.jpg"></v-img></v-card>
+                    </v-flex>
+
                 </v-flex>
                 <v-flex>
                     <div class="headline mt-3 brown--text font-weight-bold">Upload Audio Content</div>
-                    <p class="subheading mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+
+                    <v-flex xs12 sm5 md3 justify-end>
+                      <v-card tile>
+                        <v-card-text >using your google account or any social link them up and get started</v-card-text>
+
+                      </v-card>
+                    </v-flex>
+
+                    <v-flex offset-sm8 xs4>
+
+                      <v-card><v-img src="http://albvt.pythonanywhere.com/media/cover/_105651842_02357.jpg"></v-img></v-card>
+                    </v-flex>
                 </v-flex>
                 <v-flex>
                     <div class="headline mt-3 brown--text font-weight-bold">Sell and go wild</div>
-                    <p class="subheading mt-3">Nullam nec massa eu est fringilla lobortis. Praesent in enim in justo blandit varius. Cras placerat arcu in sapien rhoncus aliquet. Sed interdum tortor et tincidunt condimentum. Etiam consequat mi leo, in suscipit odio scelerisque molestie. Nam et purus consequat, iaculis augue vel, sagittis ligula. Vestibulum aliquet vulputate erat. Phasellus id mauris mauris. Nunc a maximus dolor. Curabitur ut vestibulum arcu. Curabitur non lacus justo.</p>
+                    <v-flex xs12 sm5 md3 justify-end>
+                      <v-card tile>
+                        <v-card-text >using your google account or any social link them up and get started</v-card-text>
+
+                      </v-card>
+                    </v-flex>
+
+                    <v-flex offset-sm8 xs4>
+
+                      <v-card><v-img src="http://albvt.pythonanywhere.com/media/cover/IMG_20190905_111131.jpg"></v-img></v-card>
+                    </v-flex>
                 </v-flex>
             </v-layout>
         </v-container>
@@ -28,3 +60,4 @@ export default {
 
 <style scoped>
 </style>
+
