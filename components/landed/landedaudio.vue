11941110db1254ec12e8c26978c601309b7c19ed
albvt <stainagaba@gmail.com>
2019-10-06 10:17:33 +0300

    Create landedaudio.vue

---
+++ components/landed/landedaudio.vue
@@ -0,0 +1,57 @@
+<template>
+<div class="container">
+  <v-container
+    fluid
+    grid-list-md
+    pa-2
+  >
+    <v-layout
+     align-center
+     justify-center
+     row
+     >
+      <v-flex
+        xs12
+        sm6
+        md12
+        lg7
+        xl6
+        offset-sm3
+        >
+        <v-card
+        color="grey darken-2"
+        raised
+        class="rounded-card"
+        router
+        :to="'/audio/'+audio.id"
+        >
+        <v-img :src="audio.AudioFile_cover" height="200px"></v-img>
+        <div class="container">
+<audio controls>
+  <source :src="audio.AudioFile" type="audio/ogg">
+    Your browser does not support the audio element.
+  </audio>
+</div>
+        <v-card-text>{{audio.AudioFile_tag}}</v-card-text>
+        </v-img>
+          <span>{{audio.AudioFile_caption}}</span>
+        </v-card>
+
+
+      </v-flex>
+    </v-layout>
+  </v-container>
+</div>
+
+</template>
+<script>
+export default {
+  name:"landedaudio",
+    props: ["audio",],
+};
+</script>
+<style>
+.rounded-card{
+    border-radius:10px;
+}
+</style>
