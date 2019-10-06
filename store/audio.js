0c932264c377dff6d55bf164a9060b3f29158b38
albvt <stainagaba@gmail.com>
2019-10-06 20:11:49 +0300

    Update audio.js

--- store/audio.js
+++ store/audio.js
@@ -5,22 +5,29 @@ export const state = ()=>({
       id:'1',
       AudioFile_tag:'jam_session',
       AudioFile_caption:'All reggae',
-      AudioFile:'http://127.0.0.1:8000/media/Jam_session_eps3_all_reggae.mp3',
-      AudioFile_cover:'http://127.0.0.1:8000/media/cover/Screenshot_20190907-081528.png'
+      AudioFile:'http://albvt.pythonanywhere.com/media/Africa_Is_The_Future_-_Directors_Cut.mp3',
+      AudioFile_cover:'http://albvt.pythonanywhere.com/media/cover/_105651842_02357.jpg'
     },
     {
       id:'2',
       AudioFile_tag:'jam_session',
       AudioFile_caption:'chill vibes jumanji style',
-      AudioFile:'http://127.0.0.1:8000/media/Jam_Session_Ep5.mp3',
-      AudioFile_cover:'http://127.0.0.1:8000/media/cover/Screenshot_20190907-081528_2QLBSLp.png'
+      AudioFile:'http://albvt.pythonanywhere.com/media/Jam_Session_Eps_IV_EDM__hearthis.at.mp3',
+      AudioFile_cover:'http://albvt.pythonanywhere.com/media/cover/blackwhite3.jpg'
     },
     {
       id:'3',
       AudioFile_tag:'jam_session',
       AudioFile_caption:'chill-vives where your lighter',
-      AudioFile:'http://127.0.0.1:8000/media/Jam_Session_Ep_V.mp3',
-      AudioFile_cover:'http://127.0.0.1:8000/media/cover/Screenshot_20190907-081619.png'
+      AudioFile:'http://albvt.pythonanywhere.com/media/Jam_Session_Eps_IV_EDM__hearthis.at.mp3',
+      AudioFile_cover:'http://albvt.pythonanywhere.com/media/cover/Screenshot_20190907-081528.png'
+    },
+     {
+      id:'4',
+      AudioFile_tag:'jam_session',
+      AudioFile_caption:'chill-vives where your lighter',
+      AudioFile:'http://albvt.pythonanywhere.com/media/Jam_Session_Eps_IV_EDM__hearthis.at.mp3',
+      AudioFile_cover:'http://albvt.pythonanywhere.com/media/cover/Screenshot_20190907-081528.png'
     },
 
   ]
