175c305e2e8758a27bea128990593e2a29caf146
albvt <stainagaba@gmail.com>
2019-10-10 10:04:00 +0300

    Update homelanded.vue

--- components/landed/homelanded.vue
+++ components/landed/homelanded.vue
@@ -1,8 +1,20 @@
 <template>
     <v-container fluid fill-height class="home-hero" style="max-height: 100vh;">
         <v-layout justify-center align-center column pa-5>
-            <div class="display-2 font-weight-black black--text text-xs-center">MANSA</div>
-            <div class="display-1 font-weight-bold white--text text-xs-center"><vue-typer text='Afrika At A Stream'></vue-typer></div>
+          <v-parallax
+    dark
+    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <h1 class="display-2 font-weight-thin mb-4">MANSA</h1>
+      <h4 class="subheading">First Earthly Audio Market</h4>
+       <div class="display-1 font-weight-bold white--text text-xs-center"><vue-typer text='Afrika At A Stream'></vue-typer></div>
+    </v-row>
+  </v-parallax>
+            
         </v-layout>
     </v-container>
 </template>
@@ -14,14 +26,5 @@ export default {
 </script>
 
 <style scoped>
-.home-hero {
-    background: url('http://albvt.pythonanywhere.com/media/cover/IMG_20190901_063501.jpg') no-repeat center center ;
-     background-size: cover;
-    width: 100%;
-    height: 100%;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-}
+
 </style>
