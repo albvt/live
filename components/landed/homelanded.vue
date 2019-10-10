bad228b142a042cff49757af8d0d9a509babe8e0
albvt <stainagaba@gmail.com>
2019-10-10 07:25:23 +0300

    Update homelanded.vue

--- components/landed/homelanded.vue
+++ components/landed/homelanded.vue
@@ -2,6 +2,23 @@
     <v-container fluid fill-height class="home-hero" style="max-height: 100vh;">
         <v-layout justify-center align-center column pa-5>
             <div class="display-4 font-weight-black black--text text-xs-center">MANSA</div>
+             <vue-particles
+      color="#dedede"
+        :particleOpacity="0.7"
+        :particlesNumber="80"
+        shapeType="circle"
+        :particleSize="4"
+        linesColor="#dedede"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="3"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+      ></vue-particles>
 
             <div class="display-2 font-weight-black white--text text-xs-center mb-3">First Earthly Audio Market</div>
             <div class="display-1 font-weight-bold white--text text-xs-center"><vue-typer text='Afrika At A Stream'></vue-typer></div>
@@ -17,9 +34,13 @@ export default {
 
 <style scoped>
 .home-hero {
-    background: url('http://albvt.pythonanywhere.com/media/cover/IMG_20190901_063501.jpg');
-    background-size: cover;
+    background: url('http://albvt.pythonanywhere.com/media/cover/IMG_20190901_063501.jpg') no-repeat center center ;
+     background-size: cover;
     width: 100%;
     height: 100%;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
 }
 </style>
