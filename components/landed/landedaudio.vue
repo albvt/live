27d505186fbb7bff2e2c8c64dbc5b9175ad275ec
albvt <stainagaba@gmail.com>
2019-10-10 06:48:17 +0300

    Update landedaudio.vue

--- components/landed/landedaudio.vue
+++ components/landed/landedaudio.vue
@@ -18,27 +18,34 @@
         xl6
         offset-sm3
         >
+        <v-hover v-slot:default="{ hover }">
         <v-card
-        color="grey darken-2"
+        color="grey darken-4"
         raised
         class="rounded-card"
         router
         :to="'/audio/'+audio.id"
         >
-        <v-img :src="audio.AudioFile_cover" height="200px"></v-img>
-        <div class="container">
-<audio controls>
-  <source :src="audio.AudioFile" type="audio/ogg">
-    Your browser does not support the audio element.
-  </audio>
-</div>
+        <v-img :src="audio.AudioFile_cover" height="200px">
+          <v-expand-transition>
+      <div
+        v-if="hover"
+        class="d-flex transition-fast-in-fast-out blue darken-2 v-card--reveal display-3 white--text"
+        style="height: 100%;"
+      >
+        <v-icon>mdi-play</v-icon>
+      </div>
+    </v-expand-transition>
+        </v-img>
+
         <v-card-text>{{audio.AudioFile_tag}}</v-card-text>
         </v-img>
           <span>{{audio.AudioFile_caption}}</span>
         </v-card>
 
-
+        </v-hover>
       </v-flex>
+
     </v-layout>
   </v-container>
 </div>
@@ -54,4 +61,14 @@ export default {
 .rounded-card{
     border-radius:10px;
 }
+
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: .5;
+  position: absolute;
+  width: 100%;
+}
 </style>
+
