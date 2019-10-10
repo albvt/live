83488deba8e4de7bd7c11f00cf0461472e1f066b
albvt <stainagaba@gmail.com>
2019-10-10 08:08:54 +0300

    Update homelanded.vue

--- components/landed/homelanded.vue
+++ components/landed/homelanded.vue
@@ -2,23 +2,7 @@
     <v-container fluid fill-height class="home-hero" style="max-height: 100vh;">
         <v-layout justify-center align-center column pa-5>
             <div class="display-4 font-weight-black black--text text-xs-center">MANSA</div>
-             <vue-particles
-      color="#dedede"
-        :particleOpacity="0.7"
-        :particlesNumber="80"
-        shapeType="circle"
-        :particleSize="4"
-        linesColor="#dedede"
-        :linesWidth="1"
-        :lineLinked="true"
-        :lineOpacity="0.4"
-        :linesDistance="150"
-        :moveSpeed="3"
-        :hoverEffect="true"
-        hoverMode="grab"
-        :clickEffect="true"
-        clickMode="push"
-      ></vue-particles>
+       
 
             <div class="display-2 font-weight-black white--text text-xs-center mb-3">First Earthly Audio Market</div>
             <div class="display-1 font-weight-bold white--text text-xs-center"><vue-typer text='Afrika At A Stream'></vue-typer></div>
