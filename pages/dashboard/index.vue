580e4c37aed619fd04633837e64550906ea6be1e
albvt <stainagaba@gmail.com>
2019-10-12 08:37:48 +0300

    Add files via upload

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-layout>
+      <div>
+        <singlefile></singlefile>
+      </div>
+    </v-layout>
+  </v-container>
+
+
+</template>
+<script>
+import singlefile from '~/components/fileupload/singlefile.vue'
+  export default {
+    middleware:'auth',
+    layout:'dashboard',
+    components:{singlefile,},
+  }
+</script>
+<style>
+</style>
