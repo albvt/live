27dcb45097aa4a787b28952c085a8c8fc9da458a
albvt <stainagaba@gmail.com>
2019-10-13 07:27:00 +0300

    Add files via upload

---
+++ components/googleauth.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+
+    <div class="g-signin2" data-onsuccess="onSignIn"></div>
+
+  </div>
+</template>
+<script>
+export default{
+  name:'googleauth',
+}
+</script>
+<style>
+</style>
