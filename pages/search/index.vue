4e2429907709ebaf06a7854fe9ab6608402ae9b5
albvt <stainagaba@gmail.com>
2019-10-17 07:38:19 +0300

    Update index.vue

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -1,10 +1,57 @@
 <template>
-  <v-card><v-card-text>yeah</v-card-text></v-card>
+
+    <v-layout align-center
+     justify-center
+      >
+      <v-flex
+      xs10
+      >
+      <div data-aos="zoom-in">
+        <audiocard
+        :audio ="audio"
+        v-for="audio in audios"
+        v-bind:key="audios.id"
+        ></audiocard>
+      </div>
+
+    <audioplayer></audioplayer>
+
+      </v-flex>
+    </v-layout>
 </template>
+
 <script>
+import audiocard from '~/components/audiocard.vue'
+import audioplayer from '~/components/player/audioplayer.vue'
+import axios from 'axios'
+
+
 export default{
-layouts:'search',
+
+  head () {
+    return {
+      title: 'market',
+      meta: [
+        { hid: 'description', name: 'description', content: 'buy with bitcoin' }
+      ]
+    }
+  },
+  components:{audiocard,audioplayer},
+
+/*computed: {
+    audios(){
+      return this.$store.state.audio.all
+    }
+  },*/
+
+    async asyncData () {
+     const { data } = await axios.get('http://albvt.pythonanywhere.com/random/')
+     return { audios: data }
+   }
+
 }
 </script>
-<style>
+
+<style scoped>
+
 </style>
