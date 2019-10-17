ed2c8bc07f1e23d540459b08c0bbefd1a23d3d52
albvt <stainagaba@gmail.com>
2019-10-17 07:15:11 +0300

    Add files via upload

---
+++ layouts/search.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+          <v-app-bar
+            :clipped-left="clipped"
+            fixed
+            app
+          >
+          <v-btn
+           icon
+           to="/"
+          >
+          <v-icon>mdi-home</v-icon>
+          </v-btn>
+            <v-spacer />
+            <v-text-field
+            label="Search Db"
+
+            color="purple"
+            ></v-text-field>
+            <v-btn
+              icon
+              to="/search"
+              @click.stop="rightDrawer = !rightDrawer"
+            >
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+
+          </v-app-bar>
+          <!--include components-->
+          <nuxt/>
+
+
+
+  </v-app>
+
+</template>
+<script>
+</script>
+<style>
+</style>
