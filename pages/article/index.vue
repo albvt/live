a1b2df367109c91d1e7a955af396035a9dc09f86
albvt <stainagaba@gmail.com>
2019-11-14 08:41:16 +0300

    Add files via upload

---
+++ pages/article/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>
+      BitPay Launches Support for Ethereum
+Global Businesses Can Accept Ethereum for Payment Processing and Cross-Border Transactions
+    </h1>
+    <div>
+
+          <h3>
+            ATLANTA, Sept. 16, 2019 (GLOBE NEWSWIRE) -- BitPay, the largest global blockchain payments provider, today announced its customers will soon be able to safely, securely, and compliantly accept Ethereum. Businesses using BitPay for payment processing can accept Ethereum for purchases without the need for any integration or enhancements. Additionally, wallet users will be able to store and use Ethereum in a BitPay wallet and BitPay Prepaid Visa Card holders can top up debit cards.
+
+    “BitPay is consistently at the forefront of using cryptocurrency to solve real-world problems and offer our global customers the best options to accept and pay with multiple blockchain assets,” said Stephen Pair, CEO of BitPay. “As one of the largest crypto currencies by market cap and one used by thousands of companies, Ethereum is the next logical choice.”
+
+    BitPay offers Ethereum as a payment mechanism along with Bitcoin and Bitcoin Cash. Businesses have another settlement solution that does not rely on traditional bank wires. BitPay also offers stable coin settlement with Circle CENTRE USD//Coin (USDC), Gemini Dollar (GUSD), and Paxos Standard token (PAX) as well as in 12 currencies and direct bank deposit in 37 countries.
+
+    Businesses in developing countries have a way to sell to consumers in established markets while businesses in established markets can accept cryptocurrency from consumers in developing countries. BitPay calculates prices displayed on BitPay invoices in 150 currencies and can settle in over 200 countries. For merchants who want to be settled by bank transfer, the transfer is initiated the next business day, and cryptocurrency is settled the next day, 365 days a year.
+
+    "It is exciting to see BitPay leading the way in integrating Ethereum into global payment systems,” said Vitalik Buterin, founder and creator of Ethereum. “This truly opens up a new world of possibilities for the Ethereum ecosystem, and together we can continue to be a leading innovator for real world use cases for cryptocurrencies."
+
+    Blockchain technology was invented with the goal of revolutionizing the financial industry. By continuing to add additional cryptocurrency support, BitPay is advancing blockchain payments on a global scale, making payments faster, more secure, and less expensive. Ethereum, launched in 2015, is a blockchain enabled smart contract platform.
+
+    BitPay’s existing merchants, including Microsoft, Newegg, Dish Networks, FanDuel and Avnet, will be able to turn on support for Ethereum seamlessly without any additional integration.  BitPay plans to go live with Ethereum support in the coming weeks for BitPay merchants and BitPay wallet holders.
+
+    About BitPay
+    Founded in 2011, BitPay is the pioneer and the most experienced company in bitcoin and blockchain payments. Its suite of products enables businesses to send and receive cross border payments, also enabling consumers to manage digital assets with the BitPay Wallet and turn digital assets into dollars with the BitPay Prepaid Visa® Card. The company has offices in North America, Europe, and South America and has raised over $70 million from leading investors including Founders Fund, Index Ventures and Aquiline Technology Growth. For more information visit https://bitpay.com
+
+    The BitPay Visa® Prepaid Card is issued by Metropolitan Commercial Bank, member FDIC, pursuant to a license from Visa, U.S.A. Inc. “Metropolitan” and “Metropolitan Commercial Bank” are registered trademarks of Metropolitan Commercial Bank © 2014. Use of the Card is subject to the terms and conditions of the applicable Cardholder Agreement and fee schedule, if any.
+
+    Media Contacts:
+    Jan Jahosky
+    BitPay
+    media@bitpay.com
+
+          </h3>
+  
+
+    </div>
+  </div>
+</template>
+<script>
+</script>
+<style>
+</style>
