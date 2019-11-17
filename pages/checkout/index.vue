82c85fc8e7ece6c9f933b1f50410bbb11b16cdb7
albvt <stainagaba@gmail.com>
2019-11-18 02:47:02 +0300

    Update index.vue

--- pages/checkout/index.vue
+++ pages/checkout/index.vue
@@ -4,6 +4,23 @@
   </div>
 </template>
 <script>
+export default{
+head: {
+     title: 'checkout 🚀',
+     meta: [
+       { hid: 'description', name: 'description', content: 'buy with bitcoin,audio books,African mixtapes,podcasts' }
+     ],
+     noscript: [
+       { innerHTML: 'Body No Scripts', body: true }
+     ],
+     script: [
+       { src: '/head.js' },
+       // Supported since 1.0
+       { src: '/body.js', body: true },
+       { src: '/defer.js', defer: '' }
+     ]
+   },
+}
 </script>
 <style>
 </style>
