f957c72b7be6e0a3e2b024891ce729e0959297d9
albvt <stainagaba@gmail.com>
2019-11-18 02:41:11 +0300

    Update login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -7,5 +7,13 @@
 import auth from '~/components/auth.vue'
 export default{
   components:{auth,},
+  head () {
+    return {
+      title: 'login',
+      meta: [
+        { hid: 'description', name: 'description', content: 'login to mansaah,African mixtapes and audio ' }
+      ]
+    }
+  },
 }
 </script>
