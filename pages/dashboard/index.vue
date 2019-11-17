cd6601c34846109ccc1557d076881c4204f47db4
albvt <stainagaba@gmail.com>
2019-11-18 02:45:01 +0300

    Update index.vue

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -15,6 +15,21 @@ import singlefile from '~/components/fileupload/singlefile.vue'
     middleware:'auth',
     layout:'dashboard',
     components:{singlefile,},
+    head: {
+     title: 'dashboard ',
+     meta: [
+       { hid: 'description', name: 'description', content: 'dashboard to upload and listen to mixtapes, music,podcasts ' }
+     ],
+     noscript: [
+       { innerHTML: 'Body No Scripts', body: true }
+     ],
+     script: [
+       { src: '/head.js' },
+       // Supported since 1.0
+       { src: '/body.js', body: true },
+       { src: '/defer.js', defer: '' }
+     ]
+   },
   }
 </script>
 <style>
