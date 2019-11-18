bce0f2abe2ff12db272eecf3d495aeef7074a9ca
albvt <stainagaba@gmail.com>
2019-11-18 03:03:47 +0300

    Update _id.vue

--- pages/article/_id.vue
+++ pages/article/_id.vue
@@ -2,6 +2,17 @@
   <h1>more articles</h1>
 </template>
 <script>
+export default{
+head () {
+    return {
+      title: 'articles',
+      meta: [
+        { hid: 'description', name: 'description', content: 'buy with bitcoin,mansaah,African mixtapes' },
+          { hid: 'keywords', name: 'keywords', content: ' African images, photos, gif, gifs, memes, pictures, new pictures, reaction gifs' }
+      ]
+    }
+  },
+}
 </script>
 <style>
 </style>
