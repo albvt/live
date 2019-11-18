289ce9c7cfc13c9cf7e3b6b3c3d737c7d66fd756
albvt <stainagaba@gmail.com>
2019-11-18 10:39:13 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -11,18 +11,11 @@ export default{
     data() {
       return {
         structuredData: {
-          "@context": "https://schema.org",
-  "@type": "WebSite",
-  "url": "https://www.mansaah.com/",
-  "potentialAction": [{
-    "@type": "SearchAction",
-    "target": "https://albvt.pythonanywhere.com/search?ImageFile_tag={search_term_string}&ImageFile_caption={search_term_string}",
-    "query-input": "required name=search_term_string"
-  },{
-    "@type": "SearchAction",
-    "target": "android-app://com.example/https/query.example.com/search/?q={search_term_string}",
-    "query-input": "required name=search_term_string"
-  }]
+               "@context": "http://schema.org",
+           "@type": "WebSite",
+           "url": "https://mansaah.com",
+           "name": "mansaah",
+
         },
       }
     },
