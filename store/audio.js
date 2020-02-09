7b6b85e8e9ef9221d3c13471d58a9f30c02204ed
albvt <stainagaba@gmail.com>
2020-02-09 21:58:05 +0300

    Update audio.js

--- store/audio.js
+++ store/audio.js
@@ -5,29 +5,29 @@ export const state = ()=>({
       id:'1',
       AudioFile_tag:'jam_session',
       AudioFile_caption:'All reggae',
-      AudioFile:'http://albvt.pythonanywhere.com/media/Africa_Is_The_Future_-_Directors_Cut.mp3',
-      AudioFile_cover:'http://albvt.pythonanywhere.com/media/cover/_105651842_02357.jpg'
+      AudioFile:'https://albvt.pythonanywhere.com/media/Africa_Is_The_Future_-_Directors_Cut.mp3',
+      AudioFile_cover:'https://albvt.pythonanywhere.com/media/cover/_105651842_02357.jpg'
     },
     {
       id:'2',
       AudioFile_tag:'jam_session',
       AudioFile_caption:'chill vibes jumanji style',
-      AudioFile:'http://albvt.pythonanywhere.com/media/Jam_Session_Eps_IV_EDM__hearthis.at.mp3',
-      AudioFile_cover:'http://albvt.pythonanywhere.com/media/cover/blackwhite3.jpg'
+      AudioFile:'https://albvt.pythonanywhere.com/media/Jam_Session_Eps_IV_EDM__hearthis.at.mp3',
+      AudioFile_cover:'https://albvt.pythonanywhere.com/media/cover/blackwhite3.jpg'
     },
     {
       id:'3',
       AudioFile_tag:'jam_session',
       AudioFile_caption:'chill-vives where your lighter',
-      AudioFile:'http://albvt.pythonanywhere.com/media/Jam_Session_Eps_IV_EDM__hearthis.at.mp3',
-      AudioFile_cover:'http://albvt.pythonanywhere.com/media/cover/Screenshot_20190907-081528.png'
+      AudioFile:'https://albvt.pythonanywhere.com/media/Jam_Session_Eps_IV_EDM__hearthis.at.mp3',
+      AudioFile_cover:'https://albvt.pythonanywhere.com/media/cover/Screenshot_20190907-081528.png'
     },
      {
       id:'4',
       AudioFile_tag:'jam_session',
       AudioFile_caption:'chill-vives where your lighter',
-      AudioFile:'http://albvt.pythonanywhere.com/media/Jam_Session_Eps_IV_EDM__hearthis.at.mp3',
-      AudioFile_cover:'http://albvt.pythonanywhere.com/media/cover/Screenshot_20190907-081528.png'
+      AudioFile:'https://albvt.pythonanywhere.com/media/Jam_Session_Eps_IV_EDM__hearthis.at.mp3',
+      AudioFile_cover:'https://albvt.pythonanywhere.com/media/cover/Screenshot_20190907-081528.png'
     },
 
   ]
