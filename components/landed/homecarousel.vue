8f3cd7e8f51b8f1773b1a342178d59e936c6f178
albvt <stainagaba@gmail.com>
2020-05-05 10:47:18 +0300

    Update homecarousel.vue

--- components/landed/homecarousel.vue
+++ components/landed/homecarousel.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid class="#0E2F44" color="#0E2F44" >
+  <v-container fluid class="#000000" color="#000000" >
     <v-layout row >
       <v-flex  class="display-2 font-weight-black brown--text text-xs-center my-5">Top Trending Tapes</v-flex>
     </v-layout>
