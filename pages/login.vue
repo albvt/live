064cc7a52eed8362b7ca816b2a2d9fe954980060
albvt <stainagaba@gmail.com>
2020-05-21 09:40:37 +0300

    Update login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -9,9 +9,11 @@ export default{
   components:{auth,},
   head () {
     return {
-      title: 'login',
+      title: 'Mansaah| login',
       meta: [
-        { hid: 'description', name: 'description', content: 'login to mansaah,African mixtapes and audio ' }
+        { hid: 'description', name: 'description', content: 'login to mansaah,African mixtapes and audio,TypeBeats,African Music ' },
+              { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+              
       ]
     }
   },
