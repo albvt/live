f9ef00576c80092796bcc787762527ba4cd1f936
albvt <stainagaba@gmail.com>
2020-08-17 19:22:53 +0300

    Update dashboard.vue

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-app dark>
+  <v-app style="background: #000000;">
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
       :clipped="clipped"
-      src="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
       fixed
       app
+      color="#000000"
     >
       <v-list>
         <v-list-item
@@ -17,58 +17,62 @@
           exact
         >
           <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
+            <v-icon color="#39FF14" >{{ item.icon }}</v-icon>
           </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title v-text="item.title" />
           </v-list-item-content>
+          
         </v-list-item>
       </v-list>
+      <a style="text-decoration:none" href="/search"> <v-icon color="#18D3AF">mdi-magnify</v-icon>search db</a>
     </v-navigation-drawer>
     <v-app-bar
       :clipped-left="clipped"
       fixed
       app
+      color="#000000"
+      shrink-on-scroll
+      prominent
+      src="https://picsum.photos/1920/1080?random"
+      fade-img-on-scroll
+      scroll-target="#scrolling-techniques-5"
+      scroll-threshold="500"
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" color="#18D3AF"/>
       <v-btn
         icon
         @click.stop="miniVariant = !miniVariant"
       >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+        <v-icon color="#18D3AF">mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
       </v-btn>
-
+      
       <v-toolbar-title v-text="title" />
       <v-spacer />
-    <!--  <v-btn icon><v-icon>mdi-magnify</v-icon></v-btn>-->
-    <a href="/audio">listen<v-btn icon><v-icon>mdi-music</v-icon></v-btn></a>
+     <!-- <v-btn icon><v-icon>mdi-magnify</v-icon></v-btn>-->
+      <a style="text-decoration:none" href="/audio">listen<a/><v-btn icon color="#39FF14"><v-icon>mdi-music</v-icon></v-btn>
     </v-app-bar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
+    
+    <v-footer
+      dark
+      padless
     >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+      <myfooter>
+
+      </myfooter>
+    </v-footer>
+
+   
   </v-app>
 </template>
 
 <script>
+import  myfooter from './myfooter.vue'
 export default {
   data () {
     return {
@@ -81,22 +85,20 @@ export default {
           title: 'Welcome',
           to: '/'
         },
-        {
-          icon: 'mdi-magnify',
-          title: 'search Db',
-          to: '/search'
-        },
+        
         {
           icon: 'mdi-campfire',
-          title: 'top djs',
-          to: '/user'
+          title: 'dashboard',
+          to: '/dashboard'
         },
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Mansa'
+      title: 'Mansaah',
+      components:{myfooter},
     }
   }
 }
 </script>
+
